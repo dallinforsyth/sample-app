bd037126c9c0cb3a6f7be7d73ab37c04c94e9163
dallinforsyth <dallfor192@gmail.com>
2023-01-13 13:52:20 -0600

    added /goodbye_path to route

--- app/controllers/example_pages_controller.rb
+++ app/controllers/example_pages_controller.rb
@@ -2,4 +2,8 @@ class ExamplePagesController < ApplicationController
   def hello_method
     render json: { message: "hello" }
   end
+
+  def goodbye_method
+    render json: { message: "good_bye" }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   # root "articles#index"
   get "/hello_path", controller: "example_pages",
                      action: "hello_method"
+  get "/goodbye_path", controller: "example_pages",
+                       action: "goodbye_method"
 end
